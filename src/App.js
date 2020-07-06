73c7ca0b9b0592e07ecd190834f54d946d546464
Sandra <oawx72@gmail.com>
2020-07-06 16:28:36 +0200

    la fonction recherche est faite et le formulaire d'ajout aussi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import movies from './movies.json';
 
 function App() {
+    const [currentMovie,setCurrentMovie] = useState("");
+    const [newMovie,setNewMovie]= useState(movies);
+
+    function searchMovies (event) {
+        setCurrentMovie(event.target.value);
+    }
+
+    function filterByName(){
+        if( currentMovie == ""){
+            return newMovie.map((movie,id)=><li key={id}>{movie.name} {movie.year}</li>);
+        } else {
+            const moviesFound = newMovie.filter(movie => movie.name.toLowerCase().indexOf(currentMovie.toLowerCase()) !==-1);
+            return moviesFound.map((movie,id)=><li key={id}>{movie.name} {movie.year}</li>);
+        }
+    }
+
+    function addMovie (event) {
+        event.preventDefault();
+        if(event.target !== "") {
+            const movie = {
+                name: event.target.title.value,
+                year: event.target.year.value
+            };
+            setNewMovie([... newMovie,movie]);
+        }
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <label>Recherche film :</label>
+      <input onChange={searchMovies} value={currentMovie}/>
+      <div>
+          <p>Pour ajouter un film :</p>
+          <form onSubmit={addMovie}>
+              <label>Name :</label>
+              <input type="text" name="title"></input> <br/>
+              <label>Year :</label>
+              <input type="text" name="year"></input> <br/>
+              <input type="submit"/>
+          </form>
+      </div>
+      <ul>{filterByName()}</ul>
     </div>
   );
 }
